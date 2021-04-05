8dc27439974ccb6bb52e651ccd7157ee752a6cd6
Kirill Slusarev <sliusariev2000@mail.ru>
2021-04-06 00:32:08 +0700

    Добавьте файлы проекта.

---
+++ Dz3arraysС++++/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Dz3arrays
+{
+    class Program
+    {
+        static int TryParseInput()
+        {
+            int proverka;
+
+            while (!int.TryParse(Console.ReadLine(), out proverka))
+            {
+                Console.WriteLine("!Значение не является целым числом!");
+            }
+            return proverka;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите более двух элементов массива:");
+
+            int ArrLen = TryParseInput();
+
+            while (ArrLen < 2)
+
+            {
+                Console.WriteLine("!Введите больше двух элементов!");
+
+                ArrLen = TryParseInput();
+            }
+
+            Console.WriteLine($"Длина массива: {ArrLen}");
+
+            int[] Array = new int[ArrLen];
+
+            Console.WriteLine("Ручной ввод значений");
+
+            for (int i = 0; i < ArrLen; i++)
+            {
+                Console.WriteLine($"Введите элемент массива № {i + 1}");
+
+                Array[i] = TryParseInput();
+            }
+
+            Console.WriteLine("Вывод массива:");
+
+            foreach (int Val in Array)
+            {
+                Console.Write($"{Val} ");
+            }
+            Console.WriteLine();
+
+            int largestVal = 0;
+
+            int secondLargestVal = 0;
+
+            foreach (var Val in Array) if (Val > largestVal) largestVal = Val;
+
+            Console.WriteLine($"Самое наибольшое значение: {largestVal}");
+
+            foreach (var Val in Array)
+            {
+                if (Val > secondLargestVal && Val < largestVal) secondLargestVal = Val;
+            }
+            Console.WriteLine($"Второе наибольшее значение: {secondLargestVal}");
+        }
+    }
+}
